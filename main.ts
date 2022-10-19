9c58c18f07f4eeebe3eda5fc7bc1eec0e4466ee2
avatechhkdev <52652330+avatechhkdev@users.noreply.github.com>
2022-10-19 23:22:01 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 function initAll () {
-    b1 = 0
-    b2 = 0
-    b3 = 0
-    b4 = 0
-    b5 = 0
-    b6 = 0
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P12, 1)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
     running = 0
     list = [
     1,
@@ -21,48 +21,12 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
 let r = 0
 let list: number[] = []
 let running = 0
-let b6 = 0
-let b5 = 0
-let b4 = 0
-let b3 = 0
-let b2 = 0
-let b1 = 0
 initAll()
 basic.forever(function () {
     if (running == 1) {
         basic.showNumber(list.length)
     } else {
         basic.showIcon(IconNames.Happy)
-    }
-    if (b1 == 0) {
-        pins.digitalWritePin(DigitalPin.P1, 1)
-    } else {
-        pins.digitalWritePin(DigitalPin.P1, 0)
-    }
-    if (b2 == 0) {
-        pins.digitalWritePin(DigitalPin.P2, 1)
-    } else {
-        pins.digitalWritePin(DigitalPin.P2, 0)
-    }
-    if (b3 == 0) {
-        pins.digitalWritePin(DigitalPin.P12, 1)
-    } else {
-        pins.digitalWritePin(DigitalPin.P12, 0)
-    }
-    if (b4 == 0) {
-        pins.digitalWritePin(DigitalPin.P13, 1)
-    } else {
-        pins.digitalWritePin(DigitalPin.P13, 0)
-    }
-    if (b5 == 0) {
-        pins.digitalWritePin(DigitalPin.P14, 1)
-    } else {
-        pins.digitalWritePin(DigitalPin.P14, 0)
-    }
-    if (b6 == 0) {
-        pins.digitalWritePin(DigitalPin.P15, 1)
-    } else {
-        pins.digitalWritePin(DigitalPin.P15, 0)
     }
 })
 loops.everyInterval(randint(2000, 4000), function () {
@@ -72,17 +36,17 @@ loops.everyInterval(randint(2000, 4000), function () {
         } else {
             r = list.removeAt(randint(0, list.length - 1))
             if (r == 1) {
-                b1 = 1
+                pins.digitalWritePin(DigitalPin.P1, 0)
             } else if (r == 2) {
-                b2 = 1
+                pins.digitalWritePin(DigitalPin.P2, 0)
             } else if (r == 3) {
-                b3 = 1
+                pins.digitalWritePin(DigitalPin.P12, 0)
             } else if (r == 4) {
-                b4 = 1
+                pins.digitalWritePin(DigitalPin.P13, 0)
             } else if (r == 5) {
-                b5 = 1
+                pins.digitalWritePin(DigitalPin.P14, 0)
             } else {
-                b6 = 1
+                pins.digitalWritePin(DigitalPin.P15, 0)
             }
         }
     }
